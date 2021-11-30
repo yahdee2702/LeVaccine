0cca4c2bf8dd76dc9b056ca3efbfce87d599a775
yahdee2702 <yahdi2702@gmail.com>
2021-11-30 14:45:51 +0700

    Dari Yahdi, saya sudah buat detail news dan ubah sedikit di daftar vaksin dan strings

--- app/src/main/java/com/idn/levaccine/DetailNewsActivity.kt
+++ app/src/main/java/com/idn/levaccine/DetailNewsActivity.kt
@@ -2,10 +2,15 @@ package com.idn.levaccine
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.idn.levaccine.databinding.ActivityDetailNewsBinding
 
 class DetailNewsActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityDetailNewsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail_news)
+        binding = ActivityDetailNewsBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
     }
 }
