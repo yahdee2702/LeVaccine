a5f6053da4ceee83efd3134d9834848df538d7f7
yahdee2702 <yahdi2702@gmail.com>
2021-12-04 10:14:13 +0700

    Dari Yahdi

--- app/src/main/java/com/idn/levaccine/MainActivity.kt
+++ app/src/main/java/com/idn/levaccine/MainActivity.kt
@@ -32,14 +32,6 @@ class MainActivity : AppCompatActivity() {
 
         TabLayoutMediator(binding.mainTabs,binding.vpMain) { tab, position ->
             tab.text = tabList[position]
-            when(tab.position){
-                0 -> {
-                    tab.setIcon(R.drawable.ic_logo)
-                }
-                1 -> {
-                    tab.setIcon(0)
-                }
-            }
         }.attach()
     }
 }
